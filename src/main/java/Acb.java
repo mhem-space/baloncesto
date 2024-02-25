59b4e18417ea00e9c2ef63a85ed677aee20eb8dc
mohamed.majjaoui@edu.uah.es <mohamed.majjaoui@edu.uah.es>
2024-02-25 18:03:10 +0100

    Añadida vista para Ver Votos

--- src/main/java/Acb.java
+++ src/main/java/Acb.java
@@ -1,9 +1,13 @@
 
 import java.io.*;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Acb extends HttpServlet {
 
@@ -20,11 +24,29 @@ public class Acb extends HttpServlet {
         if (req.getParameter("resetVotes") != null) {
             bd.resetearVotos();
             // Crear una cookie para indicar que se han eliminado los votos.
-            Cookie mensajeCookie = new Cookie("mensajeVotos", URLEncoder.encode("Todos los votos han sido eliminados.", "UTF-8"));
+            Cookie mensajeCookie = new Cookie("mensajeVotos",
+                    URLEncoder.encode("Todos los votos han sido eliminados.", "UTF-8"));
             mensajeCookie.setMaxAge(60); // Expira en 60 segundos
             res.addCookie(mensajeCookie);
 
             res.sendRedirect("index.html"); // Redirige de vuelta a la página principal.
+            return; // Finaliza la ejecución para no procesar más código.
+        }
+
+        if (req.getParameter("verVotos") != null) {
+            List<String> nombres = new ArrayList<>();
+            List<Integer> votos = new ArrayList<>();
+            List<Jugador> datosJugadores = bd.getJugadores();
+
+            for (Jugador jugador : datosJugadores) {
+                nombres.add(jugador.getNombre());
+                votos.add(jugador.getVotos());
+            }
+
+            s.setAttribute("nombres", nombres);
+            s.setAttribute("votos", votos);
+
+            res.sendRedirect("VerVotos.jsp"); // Redirige a la página correspondiente para ver los votos.
             return; // Finaliza la ejecución para no procesar más código.
         }
 
---
+++ src/main/webapp/VerVotos.jsp
@@ -0,0 +1,24 @@
+<%@ page import="java.util.List" %>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Votos</title>
+    <link href="estilos.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <h1>Votos de los jugadores</h1>
+    <table>
+        <tr>
+            <th>Nombre del jugador</th>
+            <th>Nº de votos</th>
+        </tr>
+        <% for (int i = 0; i < ((List<String>) session.getAttribute("nombres")).size(); i++) { %>
+        <tr>
+            <td><%= ((List<String>) session.getAttribute("nombres")).get(i)%></td>
+            <td><%= ((List<Integer>) session.getAttribute("votos")).get(i) %></td>
+        </tr>
+        <% } %>
+    </table>
+    <a href="index.html"> Ir a la página principal</a>
+</body>
+</html>
